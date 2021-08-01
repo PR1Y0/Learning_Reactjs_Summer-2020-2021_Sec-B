1fc942d628bf40df44c5e5ad006ed832e9ebe1f4
Pryam Banik <priyojitbanik25@gmail.com>
2021-08-02 01:00:02 +0600

    1R

--- Learning_ReactJS/index.js
+++ Learning_ReactJS/index.js
@@ -1,1 +1,22 @@
-console.log('hello to my programming world');
+// console.log('hello to my programming world');
+
+"use strict"
+//a=10;
+//var b=2;                                            //scope specific
+
+let c=10;                                           //block specific        
+const e=50;
+
+// let student = [1,3,4,'priyojit'];
+// console.log(student[1]);
+
+// function sum(a, b){
+//     return a+b;
+// }
+
+// const sum =function(a,b){
+//     return a+b;
+// }
+
+
+console.log(sum);
